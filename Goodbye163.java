529f7fe4de66402cf71f71f38bbb4b3c151934bc
Essence Cain <essencecain@essences-mbp.ncat.edu>
2018-11-19 15:51:10 -0500

    Create file

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/**
+ * Prints out Goodbye COMP 163
+ * @author evcain@aggies.ncat.edu
+ */
+public class Goodbye163 {
+ public static void main(String[] args) {
+  System.out.println("Goodbye COMP 163");
+ }
+}
